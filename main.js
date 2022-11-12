85951adedfceb831a7d951b3b129df144113606f
VivianaS14 <vasorianp@gmail.com>
2022-11-11 22:23:22 -0500

    Aside bar shopping cart

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
+/* Menus */
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamburgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+/* Carrito */
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', () => {
+    if (!aside.classList.contains('inactive')) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 });
 
 hamburgerMenu.addEventListener('click', () => {
+    if (!aside.classList.contains('inactive')) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 });
+
+menuCarritoIcon.addEventListener('click', () => {
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive');
+    } else if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive')
+    }
+    aside.classList.toggle('inactive');
+})
