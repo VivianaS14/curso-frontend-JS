c54e40454332c118bcfc19548b28603983036ca9
VivianaS14 <vasorianp@gmail.com>
2022-11-11 21:34:01 -0500

    Fusion mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive');
+});
+
+hamburgerMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
 });
